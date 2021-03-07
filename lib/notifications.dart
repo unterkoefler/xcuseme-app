ef4f46b5e427474ac2781dadb8ded7e0ec508c96
Willy Unterkoefler <w.unterkoelfer@gmail.com>
2021-03-07 18:56:05 -0500

    add android channel

---
+++ lib/notifications.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> createNotificationChannel() async {
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    'This channel is used for important notifications.', // description
+    importance: Importance.max,
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+}
