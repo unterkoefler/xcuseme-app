b3469104bb32b69c6ba7979a0ee41ab2310f21bb
Willy Unterkoefler <w.unterkoelfer@gmail.com>
2021-03-13 14:39:22 -0500

    dont use typescript

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -6,25 +6,9 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
     "google",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
   rules: {
-    "quotes": ["error", "double"],
-    "require-jsdoc": "off",
+    quotes: ["error", "double"],
   },
 };
